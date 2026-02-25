aee905c3ab84f92e932b2ae9f5153d05e5288271
Deepak Harish <harishdeepak35@gmail.com>
2026-02-25 22:21:04 +0530

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -2,7 +2,8 @@ const express = require('express');
 const { getProducts, getSingleProduct } = require('../controllers/productController');
 const router = express.Router();
 
-router.route('/products').get(getProducts);
-router.route('/product/:id').get(getSingleProduct);
+// IMPORTANT: No extra /api/v1 here
+router.get('/products', getProducts);
+router.get('/product/:id', getSingleProduct);
 
 module.exports = router;
