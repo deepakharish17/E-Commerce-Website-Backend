521e3fb821cd8d37d0035794ad0fb0f44d39d04d
Deepak Harish <harishdeepak35@gmail.com>
2026-02-25 22:22:00 +0530

    Refactor product controller to improve error handling

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,30 +1,41 @@
-const ProductModel = require('../models/productModel');
+const Product = require('../models/productModel');
 
-//Get Products API - /api/v1/products
-exports.getProducts = async (req, res, next) => {
-    const query = req.query.keyword?{ name : { 
-        $regex: req.query.keyword,
-        $options: 'i'
-     }}:{}
-    const products = await ProductModel.find(query);
-    res.json({
-        success: true,
-        products
-    })
-}
-
-//Get Single Product API - /api/v1/product/:id
-exports.getSingleProduct = async (req, res, next) => {
+exports.getProducts = async (req, res) => {
     try {
-        const product = await ProductModel.findById(req.params.id);
-        res.json({
+        const products = await Product.find();
+        res.status(200).json({
+            success: true,
+            count: products.length,
+            products
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
+
+exports.getSingleProduct = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+
+        if (!product) {
+            return res.status(404).json({
+                success: false,
+                message: "Product not found"
+            });
+        }
+
+        res.status(200).json({
             success: true,
             product
-        })
+        });
+
     } catch (error) {
-        res.status(404).json({
+        res.status(500).json({
             success: false,
-            message: 'Unable to get Product with that ID'
-        })
+            message: error.message
+        });
     }
-}
+};
